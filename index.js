c78827db4f86c60c147447c79b3bca847601489b
Ronnie <ron.howell0326@gmail.com>
2022-10-01 08:51:13 -0400

    commit tree f6a8c2d6a7afb01f00f71b2b04393bc71d5a2e9a

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your solution in this file!
+const leastFavoriteCustomer = 'ron';
+var customerName = 'bob';
+function upperCaseCustomerName(){
+   customerName = customerName.toUpperCase();
+}
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+}
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+}
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'john'
+}
